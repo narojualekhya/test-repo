441e47f8eec11344c9783c8ead4a025362b9eafd
alekhya <alekhyanarojua43@gmail.com>
2025-01-30 00:47:43 +0530

    java-maven sample1

---
+++ src/main/java/com/example/java_maven/JavaMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMavenApplication.class, args);
+	}
+
+}
